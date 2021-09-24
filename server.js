76f4fb65132f83f6ab0ab5d759b135a52339718e
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-24 14:44:01 -0400

    :hammer: Created basic server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) =>
+{
+    res.send("Hi There!");
+});
+
+app.listen(PORT, () =>
+{
+    console.log(`✅ PORT: ${PORT} 🌟`);
+});
