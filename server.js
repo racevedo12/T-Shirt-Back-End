6c89cf1f5010e6748f68a9de6f5878fc182ab870
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-26 13:50:07 -0400

    :hammer: Cors Added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/' + 'items';
+const cors = require("cors");
 
 // Mongoose configuration
 mongoose.connection.on("error", (err) => console.log(err.message + " mongo is not running"));
@@ -40,6 +41,8 @@ const seedItems =
 app.use( express.json() )
 app.use( express.urlencoded( { extended: true } ) );
 
+// Cors
+app.use( cors() );
 
 // Seeding data into db
 app.use("/seed-items", (req, res, next) =>
