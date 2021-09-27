01ced3de19c4549501a3570541f1e37f7f244aaf
anfalfadhil <87868348+anfalfadhil@users.noreply.github.com>
2021-09-27 11:05:47 -0400

    seeds created

---
+++ Models/seeds.js
@@ -0,0 +1,113 @@
+const seedItems = [
+  {
+    name: "FillerItem",
+    description: "First Item",
+    price: "3",
+    image: "https://imgur.com/",
+  },
+  {
+    name: "1/10th Ounce American Gold Eagle",
+    description:
+      "The modern Gold American Eagle series began in 1986. The obverse side is that of Lady Liberty created by Augustus Saint Gaudens, the design used for the 1907-1933 $20.00 US Double Eagle gold coin. Because the random year SKU can contain both type one and type two Gold Eagles, the coin’s reverse could depict one of two designs. The original reverse design features the classic heraldic eagle found on all silver and gold eagles since its inception in 1986. The new design, implemented in mid-2021 features a portrait of a single bald eagle intensely staring ahead, created by artist Jennie Norris.",
+    price: "210",
+    image: "https://imgur.com/GIz9Bu3",
+  },
+
+  {
+    name: "Goldseed 10x 1g Gold Bar - Argor-Heraeus (In Assay)",
+    description:
+      "With a strong Swiss identity, Argor-Heraeus is one of the world’s oldest refiners and assayers of Precious Metals. Their tamper evident packaging backs the weight and purity of this .9999 fine bar and ensures your bar has not been improperly handled.",
+    price: "1000",
+    image: "https://imgur.com/VhRbPUY",
+  },
+
+  {
+    name: "1/10th Ounce Gold Canadian Maple",
+    description:
+      "The Royal Canadian Mint Gold Maple Coin was first released in 1979 by the Royal Canadian Mint. Originally though, the coins were only minted in a purity of .999 gold. It wasn’t until 1982 that the mint switched to using .9999 purity gold in it’s coins. This was also the first year they released the Maple Leaf Coin as a fractional 1/10 oz coin.",
+    price: "200",
+    image: "https://imgur.com/1ZFMInU",
+  },
+
+  {
+    name: "Painting",
+    description:
+      "This is my second crack at fluid art! I really enjoy the process and can use my colour theory I learnt in hairdressing",
+    price: "160",
+    image: "https://i.imgur.com/aaEy7fc.jpeg",
+  },
+  {
+    name: "playstation4",
+    description:
+      " Engadget Sony's PlayStation 4 Pro is a perfect way to show off your 4K TV | Engadget, used",
+    price: "450",
+    image:
+      "https://s.yimg.com/uu/api/res/1.2/N7ZTE1ivmTK7WBQXkEg7Bw--~B/aD05Mzc7dz0xNjAwO2FwcGlkPXl0YWNoeW9u/https://s.yimg.com/uu/api/res/1.2/iuhOxlDK1hKSRRnpwBAr.g--~B/aD05Mzc7dz0xNjAwO2FwcGlkPXl0YWNoeW9u/https://o.aolcdn.com/hss/storage/midas/cb38a39d281ab8f732539c0d91ffed1f/204554828/PS4%2BPro%2Breview%2Blede%2Bfix.jpg.cf.jpg",
+  },
+  {
+    name: "T-Shirt",
+    description: " used T-Shirt, size M, Color: white and colors ",
+    price: "25",
+    image:
+      "https://www.dunhill.com/product_image/12592666vr/f/w750_be4e4e4.jpg",
+  },
+  {
+    name: "Shoes",
+    description: " used shoes like new 🤪 size: 7 ",
+    price: "1",
+    image:
+      "https://runningmagazine.ca/wp-content/uploads/2020/03/gettyimages-1090985526-170667a.jpg",
+  },
+  {
+    name: "TV",
+    description: 'TV Samsung UN32EH4003FXZA 32" LED TV Used',
+    price: "80",
+    image:
+      "https://buy-sellelectronics.com/shop/wp-content/uploads/2018/05/20180528_162313-e1527544116667.jpg",
+  },
+  {
+    name: "Twin Bed",
+    description:
+      " Signature Design by Ashley Cambeck Rustic Twin Storage Bed with 4 Drawers & Industrial Light | Value City Furniture | Platform Beds/Low Profile Beds",
+    price: "550",
+    image:
+      "https://images.furnituredealer.net/img/products%2Fsignature_design_by_ashley%2Fcolor%2Fdrystan_b192-53%2B52%2B2x50%2Bb100-11-b1.jpg",
+  },
+  {
+    name: " Coffee Maker",
+    description: "Taurus 900W Drip Coffee Maker with 15 Cup Capacity",
+    price: "60",
+    image:
+      "https://taurus-home.com/en/wp-content/uploads/sites/2/2021/03/920621000-Capuleto.jpg",
+  },
+  {
+    name: "iPhone",
+    description: "iPhone 11pro ",
+    price: "999",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/0vkUlE9CGelZsRZlY1XZZGqsZVQ=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22015272/cgartenberg_201105_4276_004.0.jpg",
+  },
+  {
+    name: "Cat Tower",
+    description:
+      "Bring a virtual tropical forest to your home with this stunning extra large cat tree tower! It's crafted with durable sage green plush fabric that res",
+    price: "164",
+    image:
+      "https://www.cozycatfurniture.com/image/cache/catalog/forest-style-large-cat-tower-tree-500x500.jpg",
+  },
+  {
+    name: "Bike",
+    description:
+      "A mix of mountain, road, and touring designs, hybrid bikes mash up specific features to create do-it-all bikes with a wide range of uses.",
+    price: "400",
+    image:
+      "https://www.rei.com/dam/content_team_080317_69661_hybrid_bikes_hero_lg.jpg",
+  },
+  {
+    name: "Car",
+    description: "Used Ford car color: black, made: 2018",
+    price: "23995",
+    image:
+      "https://www.rei.com/dam/content_team_080317_69661_hybrid_bikes_hero_lg.jpg",
+  },
+];
