61d96695c3795d8cddcdb0fe768516ca30fba007
Vanessa Su <vnessa.su@gmail.com>
2021-09-28 13:07:33 -0400

    :hammer: Add populate in get routes for Order

--- Controllers/orders.js
+++ Controllers/orders.js
@@ -6,43 +6,40 @@ const router = express.Router();
 const Order = require("../Models/Order");
 
 // Index Route
-router.get("/", (req, res, next) =>
-{
+router.get("/", (req, res, next) => {
     Order.find({})
-        .then(items => res.json(items) )
-        .catch(next)
+        .populate("items")
+        .then((items) => res.json(items))
+        .catch(next);
 });
 
 // Show Route
-router.get("/:id", (req, res, next) =>
-{
+router.get("/:id", (req, res, next) => {
     Order.findById(req.params.id)
-        .then(itemFound => res.status(200).send(itemFound) )
-        .catch(next)
+        .populate("items")
+        .then((itemFound) => res.status(200).send(itemFound))
+        .catch(next);
 });
 
 // Create Route
-router.post("/", (req, res, next) =>
-{
+router.post("/", (req, res, next) => {
     Order.create(req.body)
-        .then(itemCreated => res.status(201).send(itemCreated) )
-        .catch(next)
+        .then((itemCreated) => res.status(201).send(itemCreated))
+        .catch(next);
 });
 
 // Update route
-router.put("/:id", (req, res, next) =>
-{
-    Order.findOneAndUpdate( {_id: req.params.id}, req.body, {new: true} )
-        .then(updatedItem => res.status(201).send(updatedItem) )
-        .catch(next)
+router.put("/:id", (req, res, next) => {
+    Order.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .then((updatedItem) => res.status(201).send(updatedItem))
+        .catch(next);
 });
 
 // Delete route
-router.delete("/:id", (req, res, next) =>
-{
-    Order.findOneAndDelete( {_id: req.params.id} )
-        .then(deletedItem => res.status(200).send(deletedItem) )
-        .catch(next)
+router.delete("/:id", (req, res, next) => {
+    Order.findOneAndDelete({ _id: req.params.id })
+        .then((deletedItem) => res.status(200).send(deletedItem))
+        .catch(next);
 });
 
 module.exports = router;
